5bc0649355ae9b67857c51a8c7bd5fb824b055df
ali <mamarajabovdddd@gmail.com>
2023-04-05 19:00:22 +0500

    Bajardim

--- is_even.py
+++ is_even.py
@@ -1,3 +1,9 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
+number = int(input("Hoxlagan son kiriting\nmen sizga toq yoki juft ekalnigini aytaman"))
+answer = (number%2)
+if answer == 0:
+    print(f"Kiritgan soniz juft {number}")
+else:
+    print(f"Kiritgan soniz toq {number}")
