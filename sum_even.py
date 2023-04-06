28bae095da93631cf6e22b911473f92af266e837
ali <mamarajabovdddd@gmail.com>
2023-04-06 11:22:04 +0500

    Bajardim

--- sum_even.py
+++ sum_even.py
@@ -4,3 +4,28 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+
+number = 1234
+
+
+a = ((number%10))
+b = (((number//10)%10))
+c = (((number//100)%10))
+d = ((number//1000))
+
+
+
+e = (((a+1)%2)*a)
+f = (((b+1)%2)*b)
+g = (((c+1)%2)*c)
+h = (((d+1)%2)*d)
+
+
+
+var_int = (e+f+g+h)
+print(var_int)
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
